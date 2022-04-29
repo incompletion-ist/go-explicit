4d3148fcb69367c2f7551bbcfc0039a051a92971
Micah Kemp <apple@micah.ws>
2022-04-28 20:30:27 -0500

    Added Value example

---
+++ value/example_value_test.go
@@ -0,0 +1,81 @@
+// Copyright 2022 Micah Kemp
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package value_test
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"go.incompletion.ist/explicit/value"
+)
+
+type monitoredValues struct {
+	temperature value.Value[float32]
+	humidity    value.Value[float32]
+}
+
+func repeatUntilCancel(ctx context.Context, fn func()) {
+	backgroundWG := sync.WaitGroup{}
+	defer backgroundWG.Wait()
+
+	backgroundWG.Add(1)
+	go func() {
+		backgroundWG.Done()
+		for {
+			select {
+			default:
+				fn()
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+}
+
+func ExampleValue() {
+	mainWG := sync.WaitGroup{}
+	defer mainWG.Wait()
+
+	myConfig := monitoredValues{}
+
+	ctx, ctxCancel := context.WithCancel(context.Background())
+	repeatUntilCancel(ctx, func() {
+		if value, err := myConfig.temperature.ValueWait(ctx); err == nil {
+			fmt.Printf("new temperature value: %v\n", value)
+		}
+	})
+	repeatUntilCancel(ctx, func() {
+		if value, err := myConfig.humidity.ValueWait(ctx); err == nil {
+			fmt.Printf("new humidity value: %v\n", value)
+		}
+	})
+
+	time.Sleep(50 * time.Millisecond)
+	myConfig.temperature.Set(10)
+	time.Sleep(50 * time.Millisecond)
+	myConfig.humidity.Set(11)
+	time.Sleep(50 * time.Millisecond)
+	myConfig.temperature.Set(12)
+	time.Sleep(50 * time.Millisecond)
+
+	ctxCancel()
+	mainWG.Wait()
+
+	// Output: new temperature value: 10
+	// new humidity value: 11
+	// new temperature value: 12
+}
